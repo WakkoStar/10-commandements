ad12bb483e286ba054db436258ff60cb470035ca
WakkoStar <46996066+WakkoStar@users.noreply.github.com>
2020-06-19 12:01:22 +0200

    Updates

---
+++ precache-manifest.1b78748792c3b6c610584dffa5876e54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5b3339b341dbd975445c7279bf2f444",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "b7a52b6f75bfa3b62a5b",
+    "url": "/my-app/static/css/main.6322f36f.chunk.css"
+  },
+  {
+    "revision": "218987bcfd5dd4b7574e",
+    "url": "/my-app/static/js/2.30070799.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.30070799.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a52b6f75bfa3b62a5b",
+    "url": "/my-app/static/js/main.fcefdd21.chunk.js"
+  },
+  {
+    "revision": "cdb49809b38e6cb6489f",
+    "url": "/my-app/static/js/runtime-main.cb7db359.js"
+  }
+]);
